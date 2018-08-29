841dd9b638498e67340fd4585e25b83e066baea1
Zachary Ulissi <zulissi@gmail.com>
2018-08-29 18:47:26 -0400

    commit HW1, update notes, move port 8888 -> 8890, update dockerfile

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -it --rm -p 8888:8888 -v  $PWD":/home/jovyan/work"   zulissi/jupyter_06625 /bin/bash -c "start.sh jupyter  notebook --NotebookApp.token='' --NotebookApp.notebook_dir=/home/jovyan/work --LatexExporter.template_file=classicm"
+sudo docker run -it --rm -p 8890:8888 -v  $PWD":/home/jovyan/work"   zulissi/jupyter_06625 /bin/bash -c "start.sh jupyter  notebook --NotebookApp.token='' --NotebookApp.notebook_dir=/home/jovyan/work --LatexExporter.template_file=classicm"
--- launch_docker_MaxOSX.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker run -it --rm -p 8888:8888 -v  "/Users/apple/Documents/GitHub/f18-06625:/home/jovyan/work"   zulissi/jupyter_06625 /bin/bash -c "start.sh jupyter  notebook --NotebookApp.token='' --NotebookApp.notebook_dir=/home/jovyan/work --LatexExporter.template_file=classicm"
-
